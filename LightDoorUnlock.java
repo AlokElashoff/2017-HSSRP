a7516089f96a30e53183c467990e1aba7233e377
System Administrator <root@Aloks-MacBook-Pro.local>
2017-06-29 14:35:51 -0700

    Unlock 3

--- LightDoorUnlock.java
+++ LightDoorUnlock.java
@@ -1,9 +1,10 @@
-import java.util.*;
 import mraa.*;
 
 public class LightDoorUnlock {
   public static void main(String[] args)
   {
-    System.out.println(73);
+    Aio light = new Aio(3);
+    float value = readFloat(light);
+    System.out.println("The reading from the phototransistor is " + value);
   }
 }
