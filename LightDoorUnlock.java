6fd41c2ec0a8bed1fd33afdeefbbe93a6af03e56
System Administrator <root@Aloks-MacBook-Pro.local>
2017-06-29 14:48:38 -0700

    Unlock5

--- LightDoorUnlock.java
+++ LightDoorUnlock.java
@@ -1,8 +1,22 @@
 import mraa.*;
 
+
 public class LightDoorUnlock {
+
+  static {
+    try {
+      System.loadLibrary("mraajava");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println(
+          "Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" +
+          e);
+      System.exit(1);
+    }
+  }
+
   public static void main(String[] args)
   {
+
     Aio light = new Aio(3);
     float value = light.readFloat();
     System.out.println("The reading from the phototransistor is " + value);
