83cea67304be8e0c589ba1e6e67b20d75ace8058
mano254 <alok.elashoff@gmail.com>
2017-06-30 15:54:34 -0700

    Temp2

--- TempSens.java
+++ TempSens.java
@@ -16,20 +16,20 @@ public class TempSens {
   }
 
   public static void main (String[] args){
-    float c;
-    float f;
-    float k;
-    float value;
+    double c;
+    double f;
+    double k;
+    double value;
 
     Aio temp = new Aio(0);
     Gpio button = new Gpio(3);
 
-    value = temp.readFloat();
+    value = temp.readDouble();
     value = value*1023;
     //c = (value-500)/10;
     //f = (9/5)*(c)+32;
 
-    k = log(((10240000/value) - 10000));
+    k = Math.log(((10240000/value) - 10000));
     k = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * k * k ))* k );
     c = k - 273.15;              // Convert Kelvin to Celsius
     f = (c * 9.0)/ 5.0 + 32.0;
